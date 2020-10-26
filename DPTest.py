98ff3c800d71b1fe59b53d7b27cca8b7cdd59213
claudyne <claudyneuy98@gmail.com>
2020-10-27 01:12:22 +0800

    bug edits

--- DPTest.py
+++ DPTest.py
@@ -33,8 +33,8 @@ user_list = []
 
 def fill_data(data, row):
     
-    month = int(row["TIME"].day)
-    day = int(row["TIME"].month)
+    month = int(row["TIME"].month)
+    day = int(row["TIME"].day)
     year = int(row["TIME"].year)
     hour = int(row["TIME"].hour)         #already in military time
     minute = int(row["TIME"].minute)
@@ -54,6 +54,7 @@ def fill_data(data, row):
     start_time = datetime.datetime(int(start[0]),int(start[1]), int(start[2]))
     end_time = datetime.datetime(int(end[0]),int(end[1]), int(end[2]))
     day_index = (old_time - start_time).days
+    print(start_time)
     
     print("day_index: " + str(day_index))
 
@@ -171,91 +172,90 @@ for row in range(len(csv_reader)):
     line_count += 1
 print(f'Processed {line_count} lines.')
 
-print(user_list[0].data[0][11][33])
 
 
-# pd.DataFrame(user_list[0].data[1]).to_csv("johnkoch.csv")
+pd.DataFrame(user_list[0].data[1]).to_csv("johnkoch.csv")
 
-# n= []
-# for x in range(0, num_days):
-#     n.append(np.zeros((24, num_sites)))
+n= []
+for x in range(0, num_days):
+    n.append(np.zeros((24, num_sites)))
 
-# for u in user_list: #adds together all the day 1 and day 2 logs
-#     n[0] = n[0] + u.data[0]
-#     n[1] = n[1] + u.data[1]
-# # pd.DataFrame(n[0]).to_csv("day0.csv")
-# # pd.DataFrame(n[1]).to_csv("day1.csv")
-# m = [np.zeros((num_user))]
+for u in user_list: #adds together all the day 1 and day 2 logs
+    n[0] = n[0] + u.data[0]
+    n[1] = n[1] + u.data[1]
+# pd.DataFrame(n[0]).to_csv("day0.csv")
+# pd.DataFrame(n[1]).to_csv("day1.csv")
+m = [np.zeros((num_user))]
 
-# for row in n[0]:
-#     m = m + row
-# ts = pd.Series(m[0])
-# ts = np.transpose(ts)
-
-
-# #DP Noising
-# scores = []
-
-# e = 0.1
-# DP = 1/(math.exp(e/2)+1)
+for row in n[0]:
+    m = m + row
+ts = pd.Series(m[0])
+ts = np.transpose(ts)
 
 
-# #insert threads here? call noise function
+#DP Noising
+scores = []
 
-# noise(user_list, DP,  0, int(len(user_list)))
+e = 0.1
+DP = 1/(math.exp(e/2)+1)
 
-# #producing noised output CSVs
-# # pd.DataFrame(user_list[0].data[1]).to_csv("johnkochDP.csv")
-# test = user_list[0].data[1]
-# n= []
-# for x in range(0, num_days):
-#     n.append(np.zeros((24, num_sites)))
 
-# for u in user_list:
-#     n[0] = n[0] + u.data[0]
-#     n[1] = n[1] + u.data[1]
-# # pd.DataFrame(n[0]).to_csv("day0DP.csv")
-# # pd.DataFrame(n[1]).to_csv("day1DP.csv") 
-# m = [np.zeros((num_user))]
+#insert threads here? call noise function
 
-# for row in n[0]:
-#     m = m + row
-# ds = pd.Series(m[0])
-# ds = np.transpose(ds)
-# fig, axs = plt.subplots(2,1)
-# fig.suptitle(F"DP value: {e}")
-# axs[0].plot(ts)
-# axs[0].set_xlabel('Location')
-# axs[0].set_ylabel('Visits (clean)')
-# axs[1].plot(ds)
-# axs[1].set_xlabel('Location')
-# axs[1].set_ylabel('Visits (noised)')
-# plt.show()     
+noise(user_list, DP,  0, int(len(user_list)))
 
-# ### SORTING ALGORITHM (CLEAN) ###       
-# dict_ts = {0:1}
-# for v in range(len(ts)):
-#         dict_ts[v] = ts[v]
-# dicts_ts = sorted(dict_ts.items(), key = lambda kv:kv[1], reverse = True)
-# x = 0 
-# print("top 10 max occupancies (CLEAN):")
-# while (x<10):
-#     print(f'{x+1}: loc: {dicts_ts[x][0]} pop: {dicts_ts[x][1]}')
-#     x = x + 1        
+#producing noised output CSVs
+# pd.DataFrame(user_list[0].data[1]).to_csv("johnkochDP.csv")
+test = user_list[0].data[1]
+n= []
+for x in range(0, num_days):
+    n.append(np.zeros((24, num_sites)))
+
+for u in user_list:
+    n[0] = n[0] + u.data[0]
+    n[1] = n[1] + u.data[1]
+# pd.DataFrame(n[0]).to_csv("day0DP.csv")
+# pd.DataFrame(n[1]).to_csv("day1DP.csv") 
+m = [np.zeros((num_user))]
+
+for row in n[0]:
+    m = m + row
+ds = pd.Series(m[0])
+ds = np.transpose(ds)
+fig, axs = plt.subplots(2,1)
+fig.suptitle(F"DP value: {e}")
+axs[0].plot(ts)
+axs[0].set_xlabel('Location')
+axs[0].set_ylabel('Visits (clean)')
+axs[1].plot(ds)
+axs[1].set_xlabel('Location')
+axs[1].set_ylabel('Visits (noised)')
+plt.show()     
+
+### SORTING ALGORITHM (CLEAN) ###       
+dict_ts = {0:1}
+for v in range(len(ts)):
+        dict_ts[v] = ts[v]
+dicts_ts = sorted(dict_ts.items(), key = lambda kv:kv[1], reverse = True)
+x = 0 
+print("top 10 max occupancies (CLEAN):")
+while (x<10):
+    print(f'{x+1}: loc: {dicts_ts[x][0]} pop: {dicts_ts[x][1]}')
+    x = x + 1        
     
-# ### SORTING ALGORITHM (NOISED) ###
-# dict_ds = {0:1}
-# for v in range(len(ds)):
-#         dict_ds[v] = ds[v]
-# dicts_ds = sorted(dict_ds.items(), key = lambda kv:kv[1], reverse = True)
-# x = 0 
-# print("top 10 max occupancies (NOISED):")
-# while (x<10):
-#     print(f'{x+1}: loc: {dicts_ds[x][0]} pop: {dicts_ds[x][1]}')
-#     x = x + 1        
-# y = 0
-# for w in range(10):
-#     for x in range(10):
-#         if (dicts_ts[w][0] == dicts_ds[x][0]):
-#             y = y + 1 
-# print(f'score: {y}')
+### SORTING ALGORITHM (NOISED) ###
+dict_ds = {0:1}
+for v in range(len(ds)):
+        dict_ds[v] = ds[v]
+dicts_ds = sorted(dict_ds.items(), key = lambda kv:kv[1], reverse = True)
+x = 0 
+print("top 10 max occupancies (NOISED):")
+while (x<10):
+    print(f'{x+1}: loc: {dicts_ds[x][0]} pop: {dicts_ds[x][1]}')
+    x = x + 1        
+y = 0
+for w in range(10):
+    for x in range(10):
+        if (dicts_ts[w][0] == dicts_ds[x][0]):
+            y = y + 1 
+print(f'score: {y}')
